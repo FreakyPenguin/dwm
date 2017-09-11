b9dc71301569307384b2a807c110f128db27d5b4
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-09-11 10:44:16 -0700

    add lock key

--- config.h
+++ config.h
@@ -59,6 +59,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvtc", NULL };
+static const char *lockcmd[]  = { "xset", "s", "activate", NULL };
 
 #include "selfrestart.c"
 
@@ -98,6 +99,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
         { MODKEY|ShiftMask,             XK_r,      self_restart,   {0} },
+	{ MODKEY,                       XK_x,      spawn,          {.v = lockcmd } },
 };
 
 /* button definitions */
